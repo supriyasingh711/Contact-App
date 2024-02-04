6cd5a75b9683cd64861332a72023dd6c5ae6fa94
Supriya 007 <singhsupriya711@gmail.com>
2024-02-05 00:02:36 +0530

    builded

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build:{
-    outdir:"./docs"
-  }
-  ,
+    outDir:"./docs"
+  },
+  plugins: [react()],
+  
   base:"/Contact-App/"
 })
